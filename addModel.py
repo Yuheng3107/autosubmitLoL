f72aa77aff701a9b94f4215da0c0d08e8f4fa49a
Kuang Yu Heng <yuheng3107@gmail.com>
2024-02-27 21:29:02 +0800

    Tested, everything working as intended

--- addModel.py
+++ addModel.py
@@ -2,25 +2,16 @@ import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from datetime import datetime
-from login import login
 import time
-model_url = 'https://d1lojwke7j5vfp.cloudfront.net/models/create'
+
 
 chrome_options = webdriver.ChromeOptions()
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15'
 chrome_options.add_argument(f'user-agent={user_agent}')
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(1)
 
-model_url = "https://d1lojwke7j5vfp.cloudfront.net/models/create"
-model_name = "test_model"
-training_job_name = "team45-mingjun-6"
-
-def add_model(driver, model_name: str, training_job_name: str):
-    driver.get(model_url)
+def add_model(driver, create_model_url: str, model_name: str, training_job_name: str):
+    driver.get(create_model_url)
     page = driver.find_element(By.ID, 'root')
     inputs = page.find_elements(By.TAG_NAME, "input")
     name_input = inputs[0]
@@ -38,18 +29,4 @@ def add_model(driver, model_name: str, training_job_name: str):
     # Register model button is button 8
     register_model_button = buttons[8]
     register_model_button.click()    
-
-try:
-    # Log In
-    login(driver)
-    # Wait for login
-    time.sleep(1)
-    add_model(driver, model_name, training_job_name)
-    
-
-
-finally:
-    # Close the browser window when done
-    input("Press Enter to close the browser window.")
-    driver.quit()
 
---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from datetime import datetime
+from login import login
+from addModel import add_model
+import time
+model_url = 'https://d1lojwke7j5vfp.cloudfront.net/models/create'
+
+chrome_options = webdriver.ChromeOptions()
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15'
+chrome_options.add_argument(f'user-agent={user_agent}')
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(1)
+
+create_model_url = "https://d1lojwke7j5vfp.cloudfront.net/models/create"
+model_name = "test_model"
+training_job_name = "team45-mingjun-6"
+
+
+
+try:
+    # Log In
+    login(driver)
+    # Wait for login
+    time.sleep(1)
+    # This is for adding model
+    # add_model(driver, create_model_url, model_name, training_job_name)
+    
+
+
+finally:
+    # Close the browser window when done
+    input("Press Enter to close the browser window.")
+    driver.quit()
+
