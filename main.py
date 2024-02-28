9932f07a8a63973d2d23517ed4aa0e67a16a729c
Kuang Yu Heng <yuheng3107@gmail.com>
2024-02-28 17:29:48 +0800

    Made main into a function

--- main.py
+++ main.py
@@ -6,15 +6,9 @@ from login import login
 from addModel import add_model
 from submitModel import submit_model
 from readData import read_data
+import schedule
 import time
 model_url = 'https://d1lojwke7j5vfp.cloudfront.net/models/create'
-
-chrome_options = webdriver.ChromeOptions()
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15'
-chrome_options.add_argument(f'user-agent={user_agent}')
-
-driver = webdriver.Chrome()
-driver.implicitly_wait(2)
 
 create_model_url = "https://d1lojwke7j5vfp.cloudfront.net/models/create"
 model_name = "test_model"
@@ -28,39 +22,40 @@ account_name = "team45-yh"
 
 username = 'test_acc'
 password = 'Password1234!'
-try:
-    # for i in range(20):
-        
-    #     login(driver, username, password)
-    #     time.sleep(1)
-    #     submit_model(driver, submit_model_url)
-    #     time.sleep(16*60)
+def main():
+    chrome_options = webdriver.ChromeOptions()
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15'
+    chrome_options.add_argument(f'user-agent={user_agent}')
+
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(2)
+
+    try:
+        # for i in range(20):
+            
+        #     login(driver, username, password)
+        #     time.sleep(1)
+        #     submit_model(driver, submit_model_url)
+        #     time.sleep(16*60)
 
 
-    # Log In
-    login(driver, username, password)
-    # Wait for login
-    time.sleep(1)
-    # This is for adding model
-    # add_model(driver, create_model_url, model_name, training_job_name)
-    #submit_model(driver, submit_model_url)
+        # Log In
+        login(driver, username, password)
+        # Wait for login
+        time.sleep(1)
+        # This is for adding model
+        # add_model(driver, create_model_url, model_name, training_job_name)
+        submit_model(driver, submit_model_url)
+        read_data(driver, read_leaderboard_url, account_name)
 
-    read_data(driver, read_leaderboard_url, account_name)
+    finally:
+        # Close the browser window when done
+        input("Press Enter to close the browser window.")
+        driver.quit()
 
-
-
-
-    
-    
-   
-
-
-    
-
-
-
-finally:
-    # Close the browser window when done
-    input("Press Enter to close the browser window.")
-    driver.quit()
+for i in range(20):
+    for i in range(17):
+        print(f"Minute: {i+1}")
+        time.sleep(60)
+    main()
 
