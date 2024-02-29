812aa8bf7044c2dd995ae15175bfbc26a67699ac
Kuang Yu Heng <yuheng3107@gmail.com>
2024-02-29 20:32:18 +0800

    Added count to see statistic significance of it

--- analyse.py
+++ analyse.py
@@ -11,14 +11,15 @@ def analyse():
         for row in reader:
             d[row["Model Name"]].append(int(row["Number of Wins"]))
     # Get highest and lowest for each one, average, and standard deviation
-    rows = [["Mean", "Highest","Lowest", "Standard Deviation"]]
+    rows = [["Mean", "Highest","Lowest", "Standard Deviation", "Count"]]
     for job, win_list in d.items():
         data = np.array(win_list)
         std_dev = np.std(data)
         mean = np.mean(data)
         highest = np.max(data)
         lowest = np.min(data)
-        row = [job, mean, highest, lowest, std_dev]
+        count = len(win_list)
+        row = [job, mean, highest, lowest, std_dev, count]
         rows.append(row)
     with open("analysis.csv", "w") as f:
         writer = csv.writer(f)
